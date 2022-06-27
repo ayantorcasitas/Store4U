96f4d31f36ebbf57d54b46e1913379d3810a1e3f
npm <ayantorcasitas@yahoo.com>
2022-06-27 15:07:19 -0400

    Initial Commit

---
+++ Store4U.API/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Store4U.API.Entities;
+
+namespace Store4U.API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
